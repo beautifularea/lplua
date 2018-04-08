2f1d81d335dd173e9e012c7f7f17b900c53bf92b
beautifularea <beautifularea@163.com>
2018-04-08 18:39:12 +0800

    std::string TO const char*

---
+++ copy.cc
@@ -0,0 +1,7 @@
+std::string str;
+char * writable = new char[str.size() + 1];
+std::copy(str.begin(), str.end(), writable);
+writable[str.size()] = '\0'; // don't forget the terminating 0
+
+// don't forget to free the string after finished using it
+delete[] writable;
